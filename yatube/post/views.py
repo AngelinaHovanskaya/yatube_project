99d5db53fa6f0d24b3142c1927aa740d6766106f
AngelinaHovanskaya <lina.hovanskaya@yandex.ru>
2024-06-21 00:51:29 +0300

    Добавлены urls и view-функции

--- yatube/post/views.py
+++ yatube/post/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse('Main page')
+
+
+def group_post(request, slug):
+    return HttpResponse(f'posts with the group number {slug}')
