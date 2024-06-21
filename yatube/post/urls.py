23855774976b4d12e8e05d2f2543014b57b3b0f9
AngelinaHovanskaya <lina.hovanskaya@yandex.ru>
2024-06-21 14:30:30 +0300

    подключение html

--- yatube/post/urls.py
+++ yatube/post/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('group/<slug:slug>', views.group_post),
+    path('group/<slug:slug>/', views.group_post),
 ]
--- yatube/post/views.py
+++ yatube/post/views.py
@@ -4,8 +4,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Main page')
+    template = 'posts/index.html'
+    # return HttpResponse('Main page')
+    return render(request, template)
 
 
 def group_post(request, slug):
     return HttpResponse(f'posts with the group number {slug}')
+# python manage.py runserver
